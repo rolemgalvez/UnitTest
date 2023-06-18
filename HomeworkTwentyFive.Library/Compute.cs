afdf5efc51e93767ff261cf8dce2acf77dd54442
Rolem Galvez <mogalvez@pjlhuillier.com>
2023-06-18 13:21:38 +0800

    Add project files.

---
+++ HomeworkTwentyFive.Library/Compute.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeworkTwentyFive.Library
+{
+    public static class Compute
+    {
+        public static double TakeHomePay(double basicSalary, int daysOfWork, double taxPercentage)
+        {
+            double output;
+
+            double gross = basicSalary * daysOfWork;
+            double taxDeduction = gross * (taxPercentage / 100);
+            output = gross - taxDeduction; 
+
+            return output;
+        }
+    }
+}
---
+++ HomeworkTwentyFive.Tests/ComputeTests.cs
@@ -0,0 +1,40 @@
+﻿using HomeworkTwentyFive.Library;
+using Xunit;
+
+namespace HomeworkTwentyFive.Tests
+{
+    public class ComputeTests
+    {
+        [Fact]
+        public void TakeHomePayShouldReturn8000()
+        {
+            // Arrange
+            double expected = 8000;
+
+            // Act
+            double actual = Compute.TakeHomePay(1000, 10, 20);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(100, 1, 10, 90)]
+        [InlineData(200, 2, 20, 320)]
+        [InlineData(300, 3, 30, 630)]
+        public void TakeHomePayShouldReturnCorrectOutput(
+            double basicSalary,
+            int daysOfWork,
+            double taxPercentage,
+            double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Compute.TakeHomePay(basicSalary, daysOfWork, taxPercentage);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
